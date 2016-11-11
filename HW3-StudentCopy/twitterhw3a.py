049d09fd7824137e993a048bdda3a38ad82ab0be
iankk10 <Iankk@umich.edu>
2016-11-11 17:06:31 -0500

    commit twitter solutions

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -3,6 +3,24 @@
 # hashtags #UMSI-206 #Proj3 in the tweet.
 
 # You will demo this live for grading.
+import tweepy
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+# Unique code from Twitter
+access_token = "298924039-9Qhl8wqli2Kp5e3a9dairm1oofmtjgru4ME3I0j8"
+access_token_secret = "yQZheaTgNLu0X27bZTPfMyOAi3W6E1tc6mYbtXxAvfw4k"
+consumer_key = "hBrPryhRbvaa4J2H5N5fNnQTm"
+consumer_secret = "E1XkE0LKhZsbG1CSreXN2calbdt3WDWRn00fucUwdbmT6Pdiad"
+
+# authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# text for update to use
+update_text = "Go Blue"
+# file name for upload
+file_name = "media/mens_lacrosse.jpg"
+
+# upload image with the status text and hashtags
+api.update_with_media(file_name, "#USMI-206 #Proj3 " + update_text)
--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -7,6 +7,40 @@
 
 # Be prepared to change the search term during demo.
 
+import tweepy
+from textblob import TextBlob
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Unique code from Twitter
+access_token = "298924039-9Qhl8wqli2Kp5e3a9dairm1oofmtjgru4ME3I0j8"
+access_token_secret = "yQZheaTgNLu0X27bZTPfMyOAi3W6E1tc6mYbtXxAvfw4k"
+consumer_key = "hBrPryhRbvaa4J2H5N5fNnQTm"
+consumer_secret = "E1XkE0LKhZsbG1CSreXN2calbdt3WDWRn00fucUwdbmT6Pdiad"
+
+# authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# the search term
+search_term = "2016 election"
+
+results = api.search(search_term)
+
+# count number of results returned
+num_results = 0
+# running count of subjectivity
+total_subjectivity = 0
+# running count of polarity
+total_polarity = 0
+
+for result in results:
+    print("Tweet text: " + result.text)
+    analysis = TextBlob(result.text)
+    num_results += 1
+    total_polarity += analysis.sentiment.polarity
+    total_subjectivity += analysis.sentiment.subjectivity
+
+
+print("Average subjectivity is: " + str(total_subjectivity/num_results))
+print("Average polarity is: " + str(total_polarity/num_results))
