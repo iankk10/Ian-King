d65896f78ee95fda6882702bcc814f0d82809e85
ian-king <iankk@umich.edu>
2016-11-11 15:02:52 -0500

    madlib problem solution

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -9,7 +9,39 @@
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
-print("START*******")
+import nltk
+import random
+from nltk.book import text2
+
+# get first 150 tokens
+tokens = text2.tokens[:150]
+print("Original text tokens:")
+print(tokens)
+
+# get tagged tokens, create tag map
+tagged_tokens = nltk.pos_tag(tokens)
+tag_map = {"NN": "a noun", "NNP": "a singular proper noun", "VB": "a verb", "JJ": "an adjective",
+           "VBD": "a past tense verb"}
+
+# 15% for noun and proper noun, 10% for others
+probabilities = {"NN": .15, "NNP": .15, "VB": .1, "JJ": .1, "VBD": .1}
+
+final_words = []
+
+# add spaces to non special characters -- taken from sample with added special characters in text2
+def spaced(word_or_character):
+    if word_or_character in [",", ".", "?", "!", ":", "[", "]"]:
+        return word_or_character
+    else:
+        return " " + word_or_character
 
 
-print("\n\nEND*******")
+for (word, tag) in tagged_tokens:
+    if tag not in probabilities or random.random() > probabilities[tag]:
+        final_words.append(spaced(word))
+    else:
+        modified_word = input("Please enter %s (replacing %s):\n" % (tag_map[tag], word))
+        final_words.append(spaced(modified_word))
+
+print("New text:")
+print("".join(final_words))
